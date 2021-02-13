d8ae649ddb5c22f3416bbc0a8e4500a9dc974f06
FrancescoDale <fd060862@gmail.com>
2021-02-13 11:28:00 +0100

    relazioni tra dishes e restaurants e migration relativa

--- app/Dish.php
+++ app/Dish.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Dish extends Model
 {
     protected $fillable=['name', 'ingredients', 'description', 'unit_price', 'visible', 'slug', 'img_cover'];
+
+    public function restaurant() {
+        return $this->belongTo('App\Restaurant');
+    }
 }
--- app/Restaurant.php
+++ app/Restaurant.php
@@ -11,4 +11,8 @@ class Restaurant extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
+
+    public function dishes() {
+        return $this->hasMany('App\Dish');
+    }
 }
