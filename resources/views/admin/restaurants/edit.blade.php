4bfc9b141852621934facccd7e0e4b8198d4c9e3
Marco Guidi <mark.88g@gmail.com>
2021-02-15 16:40:48 +0100

    corretto verifica checkbox categorie su edit

--- resources/views/admin/restaurants/edit.blade.php
+++ resources/views/admin/restaurants/edit.blade.php
@@ -43,7 +43,7 @@
                             <div class="form-check">
                                 @if($errors->any())
                                     <input name="categories[]" class="form-check-input" type="checkbox" value="{{ $category->id }}"
-                                    {{ in_array($restaurant->id, old('restaurants', [])) ? 'checked=checked' : '' }}>
+                                    {{ in_array($category->id, old('categories', [])) ? 'checked=checked' : '' }}>
                                 @else
                                     <input name="categories[]" class="form-check-input" type="checkbox" value="{{ $category->id }}"
                                     {{ $restaurant->categories->contains($category) ? 'checked=checked' : '' }}>
@@ -60,7 +60,7 @@
 
                     <div class="form-group">
                         <button type="submit" class="btn btn-success">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg> Crea Ristorante
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg> Aggiorna i dati del ristorante
                         </button>
                     </div>
                 </form>
