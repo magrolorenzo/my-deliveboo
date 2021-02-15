4a39bb8a56e2f869c358690f82879f16f702eae1
FrancescoDale <fd060862@gmail.com>
2021-02-15 15:15:36 +0100

    views in caso di errori

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('page-title', '403 forbidden')
+
+<style media="screen">
+    h1 {
+        padding: 150px;
+    }
+</style>
+
+@section('content')
+    <h1 class="text-capitalize text-center">403! forbidden</h1>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('page-title', '404 Not Found')
+
+<style media="screen">
+    h1 {
+        padding: 150px;
+    }
+</style>
+
+@section('content')    
+    <h1 class="text-capitalize text-center">404! not found</h1>
+@endsection
