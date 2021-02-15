1c115c6835cf41f59e0c2a25695ef2192f269ca9
Marco Guidi <mark.88g@gmail.com>
2021-02-15 16:58:34 +0100

    se esiste già un ristorante non puoi crearne un altro

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,30 +7,32 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                @if(count($restaurants)>0)
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    @foreach ($restaurants as $restaurant)
-                        <p>{{$restaurant->name}}</p>
-                        <p>{{$restaurant->address}}</p>
-                        <p>{{$restaurant->piva}}</p>
-                        <p>
-                            <a class="btn btn-warning" href="{{route('admin.restaurants.edit', ['restaurant' => $restaurant->slug])}}">
-                                Modifica Ristorante
-                            </a>
-                        </p>
-                    @endforeach
-                </div>
-                <a class="btn btn-primary" href="{{route('admin.restaurants.create')}}">
-                    Crea Ristorante
-                </a>
-
-                {{-- {{route('admin.restaurants.edit', ['slug' => $restaurant->slug])}} --}}
-
+                        @foreach ($restaurants as $restaurant)
+                            <p>{{$restaurant->name}}</p>
+                            <p>{{$restaurant->address}}</p>
+                            <p>{{$restaurant->piva}}</p>
+                            <p>
+                                <a class="btn btn-warning" href="{{route('admin.restaurants.edit', ['restaurant' => $restaurant->slug])}}">
+                                    Modifica Ristorante
+                                </a>
+                            </p>
+                        @endforeach
+                    </div>
+                @else
+                    <div>
+                        <a class="btn btn-primary" href="{{route('admin.restaurants.create')}}">
+                            Crea Ristorante
+                        </a>
+                    </div>
+                @endif
 
             </div>
         </div>
