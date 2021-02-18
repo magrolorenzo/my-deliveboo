66dec5e077b4059df40dbca69ade8f094649443c
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2021-02-18 10:35:31 +0100

    aggiunto card piatti in homepage

--- resources/js/components/Dish.vue
+++ resources/js/components/Dish.vue
@@ -1,31 +1,48 @@
 <template>
-<div>
-    <h1>{{dishes[0].name}}</h1>
-
-</div>
+    <div>
+        <div class="card">
+            <img
+                :src="url_base + dishes[0].img_cover"
+                alt=""
+                class="card-img-top"
+            />
+            <div class="card-body">
+                <h1>{{ dishes[0].name }}</h1>
+                <p class="card-text">
+                    {{ dishes[0].ingredients }}
+                </p>
+                <p class="card-text">
+                    {{ dishes[0].description }}
+                </p>
+                <p class="card-text">{{ dishes[0].unit_price }} $</p>
+                <p class="card-text">
+                    {{ dishes[0].visible }}
+                </p>
+                <a href="" class="btn btn-outline-success btn-sm">Read More</a>
+                <a href="" class="btn btn-outline-danger btn-sm"
+                    ><i class="far fa-heart"></i
+                ></a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                dishes: []
-            }
-        },
-        mounted() {
-             axios
-            .get('http://localhost:8000/api/dishes')
-            .then((dishes) => {
-                let dish = dishes.data.results;
-                this.dishes = dish;
+export default {
+    data() {
+        return {
+            dishes: [],
+            url_base: "http://localhost:8000/storage/"
+        };
+    },
+    mounted() {
+        axios.get("http://localhost:8000/api/dishes").then(dishes => {
+            let dish = dishes.data.results;
+            this.dishes = dish;
             console.log(dishes.data.results[0].name);
-            });
-        }
+        });
     }
+};
 </script>
 
-<style scoped>
-    h1{
-        color: red;
-    }
-</style>
+<style scoped></style>
--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -5,21 +5,28 @@
 
 @section('content')
 <div id="app">
-  <nav class="navbar navbar-expand-md navbar-light bg-light">
-            <div class="container d-flex justify-content-center">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container d-flex justify-content-center">
 
-                <div class="input-group w-50">
-                    <input type="text" class="form-control" placeholder="Ristoranti, tipi di cucina...">
-                        <div class="input-group-append">
-                            <button class="btn btn-secondary" type="button">
-                                <i class="fa fa-search"></i>
-                            </button>
-                        </div>
+            <div class="input-group w-50">
+                <input type="text" class="form-control" placeholder="Ristoranti, tipi di cucina...">
+                <div class="input-group-append">
+                    <button class="btn btn-secondary" type="button">
+                        <i class="fa fa-search"></i>
+                    </button>
                 </div>
-                <dish/>
-                
             </div>
+
+        </div>
     </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <dish />
+
+            </div>
+        </div>
+    </div>
 
 </div>
 @endsection
