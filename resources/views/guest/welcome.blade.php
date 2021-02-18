8042527351eaa89fb18d644b37aeefe91662ca30
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2021-02-18 12:49:05 +0100

    homepage.js non funzionante

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -2,6 +2,12 @@
 
 @section('page-title', 'pagina prova')
 
+@section('script')
+
+<script src="{{ asset('js/homepage.js') }}" defer></script>
+
+@endsection
+
 
 @section('content')
 <div id="app">
@@ -22,7 +28,24 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4 mb-4">
-                <dish />
+                <div class="card">
+                    <img :src="url_base + dishes[0].img_cover" alt="" class="card-img-top" />
+                    <div class="card-body">
+                        <h1>{{ dishes[0].name }}</h1>
+                        <p class="card-text">
+                            {{ dishes[0].ingredients }}
+                        </p>
+                        <p class="card-text">
+                            {{ dishes[0].description }}
+                        </p>
+                        <p class="card-text">{{ dishes[0].unit_price }} €</p>
+                        <p class="card-text">
+                            {{ dishes[0].visible }}
+                        </p>
+                        <a href="" class="btn btn-outline-success btn-sm">Read More</a>
+                        <a href="" class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
+                    </div>
+                </div>
 
             </div>
         </div>
