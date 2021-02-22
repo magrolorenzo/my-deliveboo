205eb2884aa20283d6c6fe48260cd4379c01ae3e
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2021-02-22 12:53:17 +0100

    aggiunto migliorie grafiche e footer

--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -7,54 +7,109 @@
 
 @section('content')
 <div id="app">
+    <div class="info-section">
 
-    {{-- Navbar con search bar --}}
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <div class="container d-flex justify-content-center">
-            <div class="input-group w-50">
-                <input type="text" class="form-control" placeholder="Ristoranti, tipi di cucina...">
-                <div class="input-group-append">
-                    <button class="btn btn-secondary" type="button">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="info-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="info-container">
 
-                    <div class="info-container-left">
-                        <h1 class="text-capitalize">{{ $restaurant->name }}</h1>
-                        <h2 class="address">{{ $restaurant->address}}</h2>
-                    </div>
-                    <div class="info-container-right">
-                        <img src="https://picsum.photos/400/400?random=1" alt="">
-                        <!-- {{asset("storage/".$restaurant->img_cover)}} -->
-                    </div>
-
-
-                </div>
-                <div class="dish-container">
-                    @foreach ($restaurant->dishes as $dish)
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="https://picsum.photos/100/100?random=1" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $dish->name }}</h5>
-                            <p class="card-text">{{ $dish->ingredients }}</p>
-                            <p class="card-text">{{ $dish->description }}</p>
-                            <h5 class="card-text"> {{ $dish->unit_price }}</h5>
-                            <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+                        <div class="info-container-left">
+                            <h1 class="text-capitalize">{{ $restaurant->name }}</h1>
+                            <h5 class="address"> Indirizzo: {{ $restaurant->address}}</h5>
                         </div>
-                    </div>
-                    @endforeach
-                </div>
-            </div>
+                        <div class="info-container-right">
+                            <img src="https://picsum.photos/400/400?random=1" alt="">
+                            <!-- {{asset("storage/".$restaurant->img_cover)}} -->
+                        </div>
 
+
+                    </div>
+                </div>
+
+            </div>
         </div>
+
     </div>
+
+    <div class="dishes-section">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="dish-container">
+                        @foreach ($restaurant->dishes as $dish)
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="https://picsum.photos/100/100?random=1" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $dish->name }}</h5>
+                                <p class="card-text">{{ $dish->ingredients }}</p>
+                                <p class="card-text">{{ $dish->description }}</p>
+                                <h5 class="card-text"> {{ $dish->unit_price }}</h5>
+                                <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="footer-container">
+
+                        <div class="card" style="width: 15rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Scopri Deliveboo</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Chi siamo</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Pressroom</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Il nostro blog</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Programmazione</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Lavora con noi</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 15rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Note legali</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Termini e condizioni</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Informativa sulla privacy</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Cookies</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 15rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Aiuto</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Contatti</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">FAQ</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Tipi di cucina</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Mappa del sito</h6>
+
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 15rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Scarica l'App</h5>
+
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </footer>
 
 </div>
 
