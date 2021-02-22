75f1b375be6428164f885efb3d291d118f752eac
Marco Guidi <mark.88g@gmail.com>
2021-02-22 22:30:20 +0100

    creato file dishes.scss, modificato stile tabella

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,54 +1,60 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <table class="table">
-                    
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">id</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Ristorante</th>
-                            <th scope="col">Prezzo unitario</th>
-                            <th scope="col">Visibile</th>
-                            <th scope="col">Modifica</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($restaurants as $restaurant)
-                            @foreach ($restaurant->dishes as $dish)
-                                <tr>
-                                    <th scope="row">{{ $dish->id }}</th>
-                                    <td>{{ $dish->name }}</td>
-                                    <td>{{ $dish->restaurant->name }}</td>
-                                    <td>{{ $dish->unit_price }} €</td>
-                                    <td>{{ $dish->visible == 0 ? "Non Visibile" : "Visibile" }}</td>
-                                    <td class="w-25">
-                                        <a href="{{ route('admin.dishes.show', ['dish' => $dish->slug]) }}" class="btn btn-outline-info">
-                                            Dettagli
-                                        </a>
-                                        <a href="{{ route('admin.dishes.edit', ['dish' => $dish->slug]) }}" class="btn btn-outline-dark">
-                                            Modifica
-                                        </a>
-                                        <form action="{{ route('admin.dishes.destroy', ['dish' => $dish->id]) }}" method="post" class="d-inline-block">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" name="button" class="btn btn-danger">Elimina</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        @endforeach
-                    </tbody>
-                </table>
-                <div>
-                    <a href="{{ route('admin.dishes.create') }}" class="btn btn-success">
-                        Aggiungi un piatto
-                    </a>
-                </div>
-            </div>
-        </div>
+    <!-- titolo pagina -->
+    <div class="title my-2">
+        <h1>
+            I miei piatti
+        </h1>
+    </div>
+
+    <!-- tabella piatti -->
+    <table class="table">
+        <!-- intestazione -->
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Ristorante</th>
+                <th scope="col">Prezzo unitario</th>
+                <th scope="col">Visibile</th>
+                <th scope="col" class="btn-column">Modifica</th>
+            </tr>
+        </thead>
+
+        <!-- righe -->
+        <tbody>
+            @foreach ($restaurants as $restaurant)
+                @foreach ($restaurant->dishes as $dish)
+                    <tr>
+                        <th scope="row">{{ $dish->id }}</th>
+                        <td>{{ $dish->name }}</td>
+                        <td>{{ $dish->restaurant->name }}</td>
+                        <td>{{ $dish->unit_price }} €</td>
+                        <td>{{ $dish->visible == 0 ? "Non Visibile" : "Visibile" }}</td>
+                        <td>
+                            <a href="{{ route('admin.dishes.show', ['dish' => $dish->slug]) }}" class="btn btn-outline-info">
+                                Dettagli
+                            </a>
+                            <a href="{{ route('admin.dishes.edit', ['dish' => $dish->slug]) }}" class="btn btn-outline-dark">
+                                Modifica
+                            </a>
+                            <form action="{{ route('admin.dishes.destroy', ['dish' => $dish->id]) }}" method="post" class="d-inline-block">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" name="button" class="btn btn-danger">Elimina</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            @endforeach
+        </tbody>
+    </table>
+
+    <!-- btn aggiungi 1 piatto -->
+    <div>
+        <a href="{{ route('admin.dishes.create') }}" class="btn btn-success">
+            Aggiungi un piatto
+        </a>
     </div>
 @endsection
--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,40 +1,33 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+    <div class="card">
 
-                <div class="card">
+        <div class="card-header">
+            <h4>Dettagli piatto</h4>
+        </div>
 
-                    <div class="card-header">
-                        <h4>Dettagli piatto</h4>
-                    </div>
-
-                    <div class="card-body">
-                        <div class="dish-infos">
-                            <p><strong>Nome Piatto:</strong> {{$dish->name}}</p>
-                            <p><strong>Nome Ristorante:</strong> {{$dish->restaurant->name}}</p>
-                            <p><strong>Ingredienti:</strong> {{$dish->ingredients}}</p>
-                            <p><strong>Descrizione:</strong> {{$dish->description? $dish->description : "/" }}</p>
-                            <p><strong>Prezzo:</strong> {{$dish->unit_price}} €</p>
-                            <p><strong>Visibilità</strong>: {{$dish->visible == 0 ? "Non Visibile" : "Visibile"}}</p>
-                        </div>
-                        <div>
-                            @if($dish->img_cover)
-                                <img src="{{asset("storage/".$dish->img_cover)}}" alt="{{$dish->name}}" class="w-50">
-                            @else
-                                <p>Nessuna immagine caricata</p>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <a href="{{ route('admin.dishes.index') }}" class="btn btn-primary">
-                        🠔 Indietro
-                    </a>
-                </div>
+        <div class="card-body">
+            <div class="dish-infos">
+                <p><strong>Nome Piatto:</strong> {{$dish->name}}</p>
+                <p><strong>Nome Ristorante:</strong> {{$dish->restaurant->name}}</p>
+                <p><strong>Ingredienti:</strong> {{$dish->ingredients}}</p>
+                <p><strong>Descrizione:</strong> {{$dish->description? $dish->description : "/" }}</p>
+                <p><strong>Prezzo:</strong> {{$dish->unit_price}} €</p>
+                <p><strong>Visibilità</strong>: {{$dish->visible == 0 ? "Non Visibile" : "Visibile"}}</p>
+            </div>
+            <div>
+                @if($dish->img_cover)
+                    <img src="{{asset("storage/".$dish->img_cover)}}" alt="{{$dish->name}}" class="w-50">
+                @else
+                    <p>Nessuna immagine caricata</p>
+                @endif
             </div>
         </div>
+    </div>
+    <div>
+        <a href="{{ route('admin.dishes.index') }}" class="btn btn-primary">
+            🠔 Indietro
+        </a>
     </div>
 @endsection
