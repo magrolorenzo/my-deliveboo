8ecca4ea08b3ae047e2b77b5306bdb2165d985f9
FrancescoDale <fd060862@gmail.com>
2021-02-22 11:09:05 +0100

    definizione variabili

--- resources/js/validation.js
+++ resources/js/validation.js
@@ -1,1 +1,13 @@
-var app = new Vue
+var app = new Vue ({
+    el:'errors-root',
+    data : {
+        errors : [],
+        name : null,
+        address : null,
+        pIva : null,
+        maxChar : 11
+    },
+    methods : {
+
+    }
+});
