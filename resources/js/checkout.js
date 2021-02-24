21e212cfd35376cf175158d3de9c72027249669f
Lorenzo Magro <magrolorenzo@gmail.com>
2021-02-24 20:12:21 +0100

    Aggiunto html, script, scss e js di braintree

--- resources/js/checkout.js
+++ resources/js/checkout.js
@@ -17,7 +17,7 @@ var app = new Vue({
     },
 
     methods: {
-        
+
         getRestaurantId() {
             this.currentRestaurantId = document.getElementById("restaurant-id").innerHTML;
         },
@@ -137,4 +137,18 @@ var app = new Vue({
         }
 
     }
+});
+
+// Braintree
+var button = document.querySelector('#submit-button');
+
+braintree.dropin.create({
+    authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+    selector: '#dropin-container'
+}, function (err, instance) {
+    button.addEventListener('click', function () {
+        instance.requestPaymentMethod(function (err, payload) {
+            // Submit payload.nonce to your server
+        });
+    })
 });
--- resources/views/guest/checkout.blade.php
+++ resources/views/guest/checkout.blade.php
@@ -3,6 +3,7 @@
 
 @section('script')
     <script src="{{ asset('js/checkout.js') }}" defer></script>
+    <script src="https://js.braintreegateway.com/web/dropin/1.26.1/js/dropin.js"></script>
 @endsection
 
 @section('content')
@@ -107,73 +108,84 @@
             <div class="row">
                 <div class="col-12 shadow">
                     <h2>Pagamento</h2>
+
+
+
+                    <div id="dropin-container"></div>
+                    <button id="submit-button" class="button button--small button--green">Purchase</button>
+
                 </div>
             </div>
         </div>
 
-        {{-- <footer>
-        <div class="container">
-        <div class="row">
-        <div class="col-12">
-        <div class="footer-container-top">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="footer-container-top">
 
-        <div class="card" style="width: 15rem;">
-        <div class="card-body">
-        <h5 class="card-title">Scopri Deliveboo</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Chi siamo</h6>
-        <h6 class="card-subtitle mb-2 text-muted">Pressroom</h6>
-        <h6 class="card-subtitle mb-2 text-muted">Il nostro blog</h6>
-        <h6 class="card-subtitle mb-2 text-muted">Programmazione</h6>
-        <h6 class="card-subtitle mb-2 text-muted">Lavora con noi</h6>
+                            <div class="card" style="width: 15rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Scopri Deliveboo</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Chi siamo</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">Pressroom</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">Il nostro blog</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">Programmazione</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">Lavora con noi</h6>
+                                </div>
+                            </div>
+
+                            <div class="card" style="width: 15rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Note legali</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Termini e condizioni</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">Informativa sulla privacy</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">Cookies</h6>
+                                </div>
+                            </div>
+
+                            <div class="card" style="width: 15rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Aiuto</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Contatti</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">FAQ</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">Tipi di cucina</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">Mappa del sito</h6>
+
+                                </div>
+                            </div>
+
+                            <div class="card" style="width: 15rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Scarica l'App</h5>
+
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="footer-container-bottom">
+                            <small>© 2021 Deliveboo</small>
+                            <div class="links">
+                                <a href=""><i class="fab fa-twitter"></i></a>
+                                <a href=""><i class="fab fa-facebook"></i></a>
+                                <a href=""><i class="fab fa-instagram"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </footer>
+
+
     </div>
-</div>
 
-<div class="card" style="width: 15rem;">
-<div class="card-body">
-<h5 class="card-title">Note legali</h5>
-<h6 class="card-subtitle mb-2 text-muted">Termini e condizioni</h6>
-<h6 class="card-subtitle mb-2 text-muted">Informativa sulla privacy</h6>
-<h6 class="card-subtitle mb-2 text-muted">Cookies</h6>
-</div>
-</div>
-
-<div class="card" style="width: 15rem;">
-<div class="card-body">
-<h5 class="card-title">Aiuto</h5>
-<h6 class="card-subtitle mb-2 text-muted">Contatti</h6>
-<h6 class="card-subtitle mb-2 text-muted">FAQ</h6>
-<h6 class="card-subtitle mb-2 text-muted">Tipi di cucina</h6>
-<h6 class="card-subtitle mb-2 text-muted">Mappa del sito</h6>
-
-</div>
-</div>
-
-<div class="card" style="width: 15rem;">
-<div class="card-body">
-<h5 class="card-title">Scarica l'App</h5>
+    {{-- Script braintree --}}
 
 
-</div>
-</div>
 
-</div>
-
-<div class="footer-container-bottom">
-<small>© 2021 Deliveboo</small>
-<div class="links">
-<a href=""><i class="fab fa-twitter"></i></a>
-<a href=""><i class="fab fa-facebook"></i></a>
-<a href=""><i class="fab fa-instagram"></i></a>
-
-</div>
-</div>
-</div>
-
-</div>
-</div>
-
-</footer> --}}
-
-
-</div>
 @endsection
