056df8d360136f29740efc6c42470cb1b595e001
FrancescoDale <fd060862@gmail.com>
2021-03-02 10:23:38 +0100

    cambiato path _footer.scss, aggiornato app.scss, creati link cliccabili ed aggiunto logo app stores

--- resources/views/partials/guest/footer.blade.php
+++ resources/views/partials/guest/footer.blade.php
@@ -8,37 +8,69 @@
                     <div class="card" style="width: 15rem;">
                         <div class="card-body">
                             <h5 class="card-title">Scopri Deliveboo</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Chi siamo</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Pressroom</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Il nostro blog</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Programmazione</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Lavora con noi</h6>
+                            <ul>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Chi siamo</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Pressroom</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Il nostro blog</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Programmazione</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Lavora con noi</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
 
                     <div class="card" style="width: 15rem;">
                         <div class="card-body">
                             <h5 class="card-title">Note legali</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Termini e condizioni</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Informativa sulla privacy</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Cookies</h6>
+                            <ul>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Termini e condizioni</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Informativa sulla privacy</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Cookies</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
 
                     <div class="card" style="width: 15rem;">
                         <div class="card-body">
                             <h5 class="card-title">Aiuto</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Contatti</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">FAQ</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Tipi di cucina</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Mappa del sito</h6>
-
+                            <ul>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Contatti</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">FAQ</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Tipi di cucina</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="card-subtitle mb-2 text-muted">Mappa del sito</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
 
                     <div class="card" style="width: 15rem;">
                         <div class="card-body">
                             <h5 class="card-title">Scarica l'App</h5>
+                            <div class="imgbox mt-4">
+                                <img src="https://iconape.com/wp-content/png_logo_vector/app-store-google-play-logo.png" alt="scarica l'app">
+                            </div>
 
 
                         </div>
